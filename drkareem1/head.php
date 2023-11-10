00b6fe5fbde3bbd6c0e45addaddfe9250b20d1d1
ForsanNemah <forsan20172017@gmail.com>
2023-11-11 01:35:32 +0300

    addedad

--- drkareem1/head.php
+++ drkareem1/head.php
@@ -103,7 +103,7 @@ r.src=n;var u=t.getElementsByTagName(s)[0];
 u.parentNode.insertBefore(r,u);})(window,document,
 'https://sc-static.net/scevent.min.js');
 
-snaptr('init', 'b03a5fe0-a066-4e30-a1e9-28ab9616adf9');
+snaptr('init', 'd2781325-81f3-43c2-bec5-920eb3b1a13d');
 
 snaptr('track', 'PAGE_VIEW');
  
